f218fe81ade056abf7cc0ce899c3ab2dcce79f48
mustafashaikh4714 <shaikhmustafa.4714@gmail.com>
2020-01-30 19:39:03 +0530

    added json-server, create stream and list all the streams

--- client/src/components/streams/StreamList.js
+++ client/src/components/streams/StreamList.js
@@ -1,7 +1,38 @@
-import React from 'react'
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { fetchStreams } from '../../actions'
 
-const StreamList = () => {
-  return <div>Stream List</div>
+class StreamList extends Component {
+  componentDidMount() {
+    this.props.fetchStreams()
+  }
+
+  renderList = () => {
+    return this.props.streams.map(stream => {
+      return (
+        <div className='item'>
+          <i className='large middle aligned icon camera' />
+          <div className='content'>
+            <b style={{ fontSize: '26px' }}>{stream.title}</b>
+            <div className='description'>{stream.description}</div>
+          </div>
+        </div>
+      )
+    })
+  }
+  render() {
+    console.log(this.props.streams)
+
+    return (
+      <div>
+        <h2>Streams</h2>
+        <div className='ui celled list'>{this.renderList()}</div>
+      </div>
+    )
+  }
 }
 
-export default StreamList
+const mapStateToProps = state => {
+  return { streams: Object.values(state.streams) }
+}
+export default connect(mapStateToProps, { fetchStreams })(StreamList)
